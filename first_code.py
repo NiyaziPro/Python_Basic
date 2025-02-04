028da229f0231ba80cc1c83e401458e95d7b4996
NiyaziPro <niyazicholak@gmail.com>
2025-02-04 19:41:49 +0100

    19:41 Tuesday 04.02.2025

--- first_code.py
+++ first_code.py
@@ -4,3 +4,11 @@ if x==y:
     print("x and y are equal")
 else:
     print("x and y are not equal")
+
+print("x is of type", type(x))
+print("y is of type", type(y))
+
+# Output: x and y are not equal
+# x is of type <class 'int'>
+# y is of type <class 'str'>
+
